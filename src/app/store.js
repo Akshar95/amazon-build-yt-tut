4c38b7db67594c958a34a951e607166c22aec0d6
Akshar Patel <patel.akshar295@gmail.com>
2021-06-06 20:30:56 +0100

    Implementing Redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
 
+//the global store set up
 export const store = configureStore({
   reducer: {
-    basket: basketReducer,
+    basket: basketReducer, //one slice of the redux store (like an onion)
   },
 });
--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -1,21 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"; //import the redix toolkit
 
 const initialState = {
-  items: [],
+  items: [], //global items variable
 };
 
 export const basketSlice = createSlice({
   name: "basket",
   initialState,
   reducers: {
-    addToBasket: (state, action) => {},
+    //Actions
+    addToBasket: (state, action) => {}, //action has a payload inside it
     removeFromBasket: (state, action) => {},
   },
 });
 
-export const { addToBasket, removeFromBasket } = basketSlice.actions;
+export const { addToBasket, removeFromBasket } = basketSlice.actions; //export as this so can use througout the application
 
 // Selectors - This is how we pull information from the Global store slice
-export const selectItems = (state) => state.basket.items;
+export const selectItems = (state) => state.basket.items; //selectItems accesses the state
 
 export default basketSlice.reducer;
