2b1de84cbbf95e687dface17fbf12be4477b9b98
Akshar Patel <patel.akshar295@gmail.com>
2021-06-06 14:47:33 +0100

    Implementing Firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCzgL87kzG01twtCghI8ZGeS_tkh-7TNSo",
+    authDomain: "build-yt-tut.firebaseapp.com",
+    projectId: "build-yt-tut",
+    storageBucket: "build-yt-tut.appspot.com",
+    messagingSenderId: "793927228892",
+    appId: "1:793927228892:web:0cb86e1629a88fb2f10319",
+    measurementId: "G-4F79H7PMJR"
+  };
