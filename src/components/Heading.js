5e26e5368586bb8b2bc34d1564ead789828abdc1
Akshar Patel <patel.akshar295@gmail.com>
2021-06-06 18:13:16 +0100

    Modifying _app.js and adding logged in User Name

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -7,6 +7,9 @@ import {
 import { signIn, signOut, useSession } from "next-auth/client";
 
 function Heading() {
+
+    const [session] = useSession();
+
     return (
         <header>
             {/* Top nav */}
@@ -33,8 +36,8 @@ function Heading() {
                {/* Right */}
                 <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
                     
-                    <div onClick={signIn} className="link">
-                        <p>Hello Akshar Patel</p>
+                    <div onClick={!session ? signIn : signOut} className="link">
+                        <p>{session ? `Hello, ${session.user.name}` : 'Sign In'}</p>
                         <p className="font-extrabold md:text-sm">Account & list</p>
                     </div>
 
