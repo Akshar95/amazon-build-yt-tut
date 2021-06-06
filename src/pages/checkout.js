77c1cdd1f07643dcb975c5040d246131dd99b2db
Akshar Patel <patel.akshar295@gmail.com>
2021-06-06 18:40:34 +0100

    Building the Checkout Page

---
+++ src/pages/checkout.js
@@ -0,0 +1,23 @@
+import Heading from "../components/Heading"
+
+function Checkout() {
+    return (
+        <div className="bg-gray-100">
+            <Heading />
+
+                <main className='lg:flex max-w-screen-xl mx-auto'>
+                    <h1>CHECK OUT PAGE</h1>
+                    
+
+
+
+
+
+                </main>
+
+           
+        </div>
+    )
+}
+
+export default Checkout
